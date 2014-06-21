19115bf6894b97e176cbd3cdd9bc0f947dcd2be2
jennirich <richards@fiu.edu>
2014-06-20 20:12:16 -0400

    committing new run_analysis script and codebook

--- run_analysis.R
+++ run_analysis.R
@@ -56,20 +56,17 @@ meanSD <- data.frame(c(data[,c(563,562,1:6,41:46,81:86,121:126,161:166,201:202,2
 
 #Read in activity labels with their associated numbers
 activity <- read.table('C:/Rrepos/Rdata/Tidy/runAnalysis/activity_labels.txt', as.is=T)
-
+ 
 #make a single dataframe of the activity numbers
 index <- data.frame(meanSD[,2])
-     
-#Replace the numbers with descriptors; this is ugly but it works; NB: could also use plyr revalue() or mapvalue() functions
-index1 <- replace(index, index[1]==1, "WALKING")
-index2 <- replace(index1, index1[1]==2, "WALKING_UPSTAIRS")
-index3 <- replace(index2, index2[1]==3, "WALKING_DOWNSTAIRS")
-index4 <- replace(index3, index3[1]==4, "SITTING")
-index5 <- replace(index4, index4[1]==5, "STANDING")
-index6 <- replace(index5, index5[1]==6, "LAYING")
+indexfin <- index   
+     for (i in 1:6){
+          index[i] <- replace(indexfin, indexfin[1]==i, activity[i,2])
+          indexfin <- index[i]
+     }
 
 #replace the activity numbers in the dataframe with the activity descriptions
-meanSD[2] <- index6
+meanSD[2] <- indexfin
 
 #make subjects and activities into factors
 meanSD[2]<- as.factor(meanSD$V1.1)
@@ -87,56 +84,36 @@ names(meanSD) <- colLabels
 #check to see if column labels are the descriptors
 head(meanSD,2)
 str(meanSD)
-
 #****************************************************************************************************
 #CREATE SECOND DATA SET WITH AVERAGE OF EACH VARIABLE BY ACTIVITY AND SUBJECT
+#Melt means prior to aggregating, then cast to get final tidy dataset
 
-#select the columns to be averaged
-cols <- c(3:68)
+#get reshape2 library
+library(reshape2)
 
-#create the average data using the aggregate function
-newData <- data.frame()
-for (i in cols){
-     x <- aggregate(meanSD[,i], by=c(meanSD[1],meanSD[2]), mean)
-     newData <- rbind(newData, x)
-}
+#melt the data.frame
+newDataM <- melt(meanSD, id=1:2)
 
-#check the new data
-head(newData,300)
+#get means for all of the variables by subject and activity types
+meanSD1 <- aggregate(newDataM[,4], by=c(newDataM[3],newDataM[1],newDataM[2]), mean)
+head(meanSD1,100)
+names(meanSD1)
 
-#create variable labels for long column of averages
-data_labels <- colLabels[3:68]
-data_labels_all <- rep(data_labels, each=180)
+#separate long format into one with separate variables by casting
+meanSD2 <- dcast(meanSD1, Subject+Type_of_Activity ~ variable)
+head(meanSD2,10)
+names(meanSD2)
 
-#checking to make sure that variable names match data
-test1 <- tapply(meanSD[,3], c(meanSD[1],meanSD[2]), mean)
-test1a <- head(newData,180)
-test2 <- tapply(meanSD[,22], c(meanSD[1],meanSD[2]), mean)
-test3 <- newData[3421:3600,]
-
-#change data label list into vector and attach to dataframe
-as.vector(data_labels_all)
-is.vector(data_labels_all)
-newData$Variable <- data_labels_all
-head(newData)
-
-#tidy up names and order of columns in dataframe
-names(newData)[3] <- "Variable_Mean"
-newData <- newData[c(4,1,2,3)]
-head(newData,10)
-str(newData)
-
-#unlist the lists in newData so that the write functions will work
-newData1 <- as.data.frame(lapply(newData, unlist))
+#****************************************************************************************************
 
 #rename dataset to a more meaninful name
-Human_Activity_Smartphone_Recognition_Variable_Means <- newData1
+Human_Activity_Smartphone_Recognition_Variable_Means <- meanSD2
 
 #save a txt file for uploading
-write.table(newData1, file = "C:/Rrepos/Rdata/Tidy/Human_Activity_Smartphone_Recognition_Variable_Means", col.names = T, sep = "\t", quote = F, row.names = F)
+write.table(meanSD2, file = "C:/Rrepos/Rdata/Tidy/newHuman_Activity_Smartphone_Recognition_Variable_Means", col.names = T, sep = ",", quote = F, row.names = F)
 
-#also save a txt file with a shorter name
-write.table(newData1, file = "C:/Rrepos/Rdata/Tidy/newData", col.names = T, sep = "\t", quote = F, row.names = F)
+#also save a txt file with the shorter name
+write.table(meanSD2, file = "C:/Rrepos/Rdata/Tidy/meanSD2", col.names = T, sep = ",", quote = F, row.names = F)
 
 
 
